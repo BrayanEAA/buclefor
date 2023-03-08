5deca49a46289304bba93cbdcc049b58835d05c0
BrayanEAA <brayaneduardoarteaga@gmail.com>
2023-03-08 16:30:24 -0600

    bucle for

---
+++ semana7/for/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("************************************\n"+
+               "=====================================\n"+
+               "Ingrese un valor entero positivo: ");
+int n = int.Parse(Console.ReadLine());
+for (int y = 1; y <= n; y++)
+{
+    for (int x = 1; x <= y; x++)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine();
+}
+Console.ReadKey();
